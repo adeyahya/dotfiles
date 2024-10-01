2df191bcce096c7badf471d7819ecd5d6a68058e
Ade Yahya <adeyahyaprasetyo@gmail.com>
2024-10-01 17:05:18 +0700

    fix nvim path

---
+++ .config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,23 @@
+return {
+  -- "xiyaowong/transparent.nvim",
+  {
+    "2nthony/vitesse.nvim",
+    dependencies = {
+      "tjdevries/colorbuddy.nvim",
+    },
+    priority = 1000,
+    config = function()
+      require("vitesse").setup({
+        transparent_background = true,
+        transparent_float_background = true,
+      })
+
+      vim.cmd.colorscheme("vitesse")
+      require("lualine").setup({
+        options = {
+          theme = "vitesse",
+        },
+      })
+    end,
+  },
+}
