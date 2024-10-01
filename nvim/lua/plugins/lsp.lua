9d94838c069eca5745deda6e441cc91fc09c927e
Ade Yahya <adeyahyaprasetyo@gmail.com>
2024-10-01 16:58:16 +0700

    init

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,7 @@
+return {
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.astro" },
+  { import = "lazyvim.plugins.extras.lang.sql" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+}
