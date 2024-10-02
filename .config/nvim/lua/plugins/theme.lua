f49e2ec3f15dd81224250d1901cd267379f72989
Ade Yahya <adeyahyaprasetyo@gmail.com>
2024-10-03 00:29:59 +0700

    update

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,24 +1,10 @@
 return {
-  -- "xiyaowong/transparent.nvim",
+  "xiyaowong/transparent.nvim",
   {
-    "2nthony/vitesse.nvim",
-    enabled = not vim.g.vscode,
-    dependencies = {
-      "tjdevries/colorbuddy.nvim",
-    },
+    "ellisonleao/gruvbox.nvim",
     priority = 1000,
     config = function()
-      require("vitesse").setup({
-        transparent_background = true,
-        transparent_float_background = true,
-      })
-
-      vim.cmd.colorscheme("vitesse")
-      require("lualine").setup({
-        options = {
-          theme = "vitesse",
-        },
-      })
+      vim.cmd.colorscheme("gruvbox")
     end,
   },
 }
