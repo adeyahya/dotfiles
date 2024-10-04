021a598e235297520e6f2a5fbe5a1d4352755bfe
Ade Yahya <adeyahyaprasetyo@gmail.com>
2024-10-05 04:39:17 +0700

    disable inlay hints

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -4,6 +4,9 @@ return {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
     config = function()
+      require("gruvbox").setup({
+        transparent_mode = true,
+      })
       vim.cmd.colorscheme("gruvbox")
     end,
   },
