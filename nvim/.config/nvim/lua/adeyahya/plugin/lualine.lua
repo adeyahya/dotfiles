fca5364ce96a51d465442a85adfb7892f6177fe7
adeyahya <adeyahyaprasetyo@gmail.com>
2025-11-04 20:20:20 +0700

    change style

--- nvim/.config/nvim/lua/adeyahya/plugin/lualine.lua
+++ nvim/.config/nvim/lua/adeyahya/plugin/lualine.lua
@@ -1,11 +1,26 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("lualine").setup({
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
       options = {
-        theme = "catppuccin",
+        component_separators = "",
+        section_separators = { left = "", right = "" },
       },
-    })
-  end,
+      sections = {
+        lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+        lualine_y = {
+          { "progress", separator = " ", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {
+          {
+            function()
+              return " " .. os.date("%R")
+            end,
+            separator = { right = "" },
+          },
+        },
+      },
+    },
+  },
 }
