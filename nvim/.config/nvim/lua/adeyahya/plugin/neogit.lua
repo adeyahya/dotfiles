d4ab005d665de7c9c9d783762cfd7f72e9de68ec
Ade Yahya Prasetyo <adeyahyaprasetyo@gmail.com>
2025-11-04 14:13:02 +0700

    update neogit

--- nvim/.config/nvim/lua/adeyahya/plugin/neogit.lua
+++ nvim/.config/nvim/lua/adeyahya/plugin/neogit.lua
@@ -1,13 +1,32 @@
 return {
   "NeogitOrg/neogit",
   dependencies = {
-    "nvim-lua/plenary.nvim", -- required
-    "sindrets/diffview.nvim", -- optional - Diff integration
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  keys = {
+    { "<leader>gs", "<cmd>Neogit<cr>", desc = "Neogit Status" },
+    { "<leader>gc", "<cmd>Neogit commit<cr>", desc = "Neogit Commit" },
+    { "<leader>gp", "<cmd>Neogit push<cr>", desc = "Neogit Push" },
+    { "<leader>gl", "<cmd>Neogit log<cr>", desc = "Neogit Log" },
+  },
+  opts = {
+    integrations = {
+      diffview = true,
+      telescope = true,
+    },
+    disable_signs = false,
+    disable_hint = false,
+    disable_context_highlighting = false,
+    disable_commit_confirmation = false,
+    auto_refresh = true,
+    disable_insert_on_commit = "auto",
 
-    -- Only one of these is needed.
-    "nvim-telescope/telescope.nvim", -- optional
-    "ibhagwan/fzf-lua", -- optional
-    "nvim-mini/mini.pick", -- optional
-    "folke/snacks.nvim", -- optional
+    signs = {
+      section = { "", "" },
+      item = { "", "" },
+      hunk = { "", "" },
+    },
   },
 }
